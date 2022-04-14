ee763b4dddbcf8f77d3e4eeaf5d0b7a1f5e48b49
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-14 13:30:09 -0600

    model and api routes defined for manipulating posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,98 @@
+const { Post, User } = require('../../models');
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+
+// GET all posts /api/posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET single post /api/posts/1
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST new post /api/posts
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT update post /api/posts/1
+// SAVE UPDATE FOR AFTER SESSION COOKIES ARE SET UP
+
+// DELETE single post /api/posts/1
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,17 @@
 const User = require('./User');
+const Post = require('./Post');
 
 // Associations here
+// User | Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = {
-    User
+    User,
+    Post
 };
