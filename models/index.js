a56f637724a8e0306716f9eb9b528aff8206a49a
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-14 13:55:27 -0600

    model for comments written and associations defined

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // Associations here
 // User | Post
@@ -11,7 +12,25 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// User | Comment | Post
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 module.exports = {
     User,
-    Post
+    Post,
+    Comment
 };
