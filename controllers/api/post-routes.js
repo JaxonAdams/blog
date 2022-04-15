9f81d2875ab7d05b3dee549629d22bb2ed4aa93d
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-15 10:01:59 -0600

    set up authguard for routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const { Post, User, Comment } = require('../../models');
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // GET all posts /api/posts
 router.get('/', (req, res) => {
@@ -75,7 +76,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST new post /api/posts
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         content: req.body.content,
@@ -89,10 +90,21 @@ router.post('/', (req, res) => {
 });
 
 // PUT update post /api/posts/1
-// SAVE UPDATE FOR AFTER SESSION COOKIES ARE SET UP
+router.put('/upvote', withAuth,  (req, res) => {
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+});
 
 // DELETE single post /api/posts/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
